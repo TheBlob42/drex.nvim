bf8160c956c0ff28732a2d5eef217186c67ff333
TheBlob42 <hessenmobbel@web.de>
2022-07-21 22:20:03 +0200

    refactor!: move config, cursor & netrw files
    Move the 'config.lua', 'cursor.lua' and 'netrw.lua' into a dedicated
    "config" directory to better structure the source code
    
    This is breaking because of the 'cursor' and 'netrw' file now need new
    require statements, but since these should only been used for internal
    stuff this should not be a big problem

--- ftplugin/drex.lua
+++ ftplugin/drex.lua
@@ -3,5 +3,5 @@ local config = require('drex.config')
 config.set_default_keybindings(0)
 
 if config.options.hide_cursor then
-    require('drex.cursor').init()
+    require('drex.config.cursor').init()
 end
