bafe6b75e9fc22628be314e22b920a5c1edd0cfa
TheBlob42 <hessenmobbel@web.de>
2022-09-06 21:50:21 +0200

    feat: pass options to keybindings

--- lua/drex/config/init.lua
+++ lua/drex/config/init.lua
@@ -38,57 +38,57 @@ local defaults = {
             -- always use visual mode linewise for better visibility
             ['v'] = 'V',
             -- open/close directories
-            ['l'] = '<cmd>lua require("drex.elements").expand_element()<CR>',
-            ['h'] = '<cmd>lua require("drex.elements").collapse_directory()<CR>',
-            ['<right>'] = '<cmd>lua require("drex.elements").expand_element()<CR>',
-            ['<left>']  = '<cmd>lua require("drex.elements").collapse_directory()<CR>',
-            ['<2-LeftMouse>'] = '<LeftMouse><cmd>lua require("drex.elements").expand_element()<CR>',
-            ['<RightMouse>']  = '<LeftMouse><cmd>lua require("drex.elements").collapse_directory()<CR>',
+            ['l'] = { '<cmd>lua require("drex.elements").expand_element()<CR>', { desc = 'expand element' }},
+            ['h'] = { '<cmd>lua require("drex.elements").collapse_directory()<CR>', { desc = 'collapse directory' }},
+            ['<right>'] = { '<cmd>lua require("drex.elements").expand_element()<CR>', { desc = 'expand element' }},
+            ['<left>']  = { '<cmd>lua require("drex.elements").collapse_directory()<CR>', { desc = 'collapse directory'}},
+            ['<2-LeftMouse>'] = { '<LeftMouse><cmd>lua require("drex.elements").expand_element()<CR>', { desc = 'expand element' }},
+            ['<RightMouse>']  = { '<LeftMouse><cmd>lua require("drex.elements").collapse_directory()<CR>', { desc = 'collapse directory' }},
             -- open files in separate windows/tabs
-            ['<C-v>'] = '<cmd>lua require("drex.elements").open_file("vs")<CR>',
-            ['<C-x>'] = '<cmd>lua require("drex.elements").open_file("sp")<CR>',
-            ['<C-t>'] = '<cmd>lua require("drex.elements").open_file("tabnew", true)<CR>',
+            ['<C-v>'] = { '<cmd>lua require("drex.elements").open_file("vs")<CR>', { desc = 'open file in vsplit' }},
+            ['<C-x>'] = { '<cmd>lua require("drex.elements").open_file("sp")<CR>', { desc = 'open file in split' }},
+            ['<C-t>'] = { '<cmd>lua require("drex.elements").open_file("tabnew", true)<CR>', { desc = 'open file in new tab' }},
             -- switch root directory
-            ['<C-l>'] = '<cmd>lua require("drex.elements").open_directory()<CR>',
-            ['<C-h>'] = '<cmd>lua require("drex.elements").open_parent_directory()<CR>',
+            ['<C-l>'] = { '<cmd>lua require("drex.elements").open_directory()<CR>', { desc = 'open directory in new buffer' }},
+            ['<C-h>'] = { '<cmd>lua require("drex.elements").open_parent_directory()<CR>', { desc = 'open parent directory in new buffer' }},
             -- manual reload
-            ['<F5>'] = '<cmd>lua require("drex").reload_directory()<CR>',
+            ['<F5>'] = { '<cmd>lua require("drex").reload_directory()<CR>', { desc = 'reload' }},
             -- jump around elements
-            ['gj'] = '<cmd>lua require("drex.actions.jump").jump_to_next_sibling()<CR>',
-            ['gk'] = '<cmd>lua require("drex.actions.jump").jump_to_prev_sibling()<CR>',
-            ['gh'] = '<cmd>lua require("drex.actions.jump").jump_to_parent()<CR>',
+            ['gj'] = { '<cmd>lua require("drex.actions.jump").jump_to_next_sibling()<CR>', { desc = 'jump to next sibling' }},
+            ['gk'] = { '<cmd>lua require("drex.actions.jump").jump_to_prev_sibling()<CR>', { desc = 'jump to prev sibling' }},
+            ['gh'] = { '<cmd>lua require("drex.actions.jump").jump_to_parent()<CR>', { desc = 'jump to parent element' }},
             -- file actions
-            ['s'] = '<cmd>lua require("drex.actions.stats").stats()<CR>',
-            ['a'] = '<cmd>lua require("drex.actions.files").create()<CR>',
-            ['d'] = '<cmd>lua require("drex.actions.files").delete("line")<CR>',
-            ['D'] = '<cmd>lua require("drex.actions.files").delete("clipboard")<CR>',
-            ['p'] = '<cmd>lua require("drex.actions.files").copy_and_paste()<CR>',
-            ['P'] = '<cmd>lua require("drex.actions.files").cut_and_move()<CR>',
-            ['r'] = '<cmd>lua require("drex.actions.files").rename()<CR>',
-            ['R'] = '<cmd>lua require("drex.actions.files").multi_rename("clipboard")<CR>',
+            ['s'] = { '<cmd>lua require("drex.actions.stats").stats()<CR>', { desc = 'show element stats' }},
+            ['a'] = { '<cmd>lua require("drex.actions.files").create()<CR>', { desc = 'create element' }},
+            ['d'] = { '<cmd>lua require("drex.actions.files").delete("line")<CR>', { desc = 'delete element' }},
+            ['D'] = { '<cmd>lua require("drex.actions.files").delete("clipboard")<CR>', { desc = 'delete (clipboard)' }},
+            ['p'] = { '<cmd>lua require("drex.actions.files").copy_and_paste()<CR>', { desc = 'copy & paste (clipboard)' }},
+            ['P'] = { '<cmd>lua require("drex.actions.files").cut_and_move()<CR>', { desc = 'cut & move (clipboard)' }},
+            ['r'] = { '<cmd>lua require("drex.actions.files").rename()<CR>', { desc = 'rename element' }},
+            ['R'] = { '<cmd>lua require("drex.actions.files").multi_rename("clipboard")<CR>', { desc = 'rename (clipboard)' }},
             -- add/remove elements from clipboard
-            ['M'] = '<cmd>DrexMark<CR>',
-            ['u'] = '<cmd>DrexUnmark<CR>',
-            ['m'] = '<cmd>DrexToggle<CR>',
-            ['cc'] = '<cmd>lua require("drex.clipboard").clear_clipboard()<CR>',
-            ['cs'] = '<cmd>lua require("drex.clipboard").open_clipboard_window()<CR>',
+            ['M'] = { '<cmd>DrexMark<CR>', { desc = 'mark element' }},
+            ['u'] = { '<cmd>DrexUnmark<CR>', { desc = 'unmark element' }},
+            ['m'] = { '<cmd>DrexToggle<CR>', { desc = 'toggle element' }},
+            ['cc'] = { '<cmd>lua require("drex.clipboard").clear_clipboard()<CR>', { desc = 'clear clipboard' }},
+            ['cs'] = { '<cmd>lua require("drex.clipboard").open_clipboard_window()<CR>', { desc = 'edit clipboard' }},
             -- string copy utilities
-            ['y'] = '<cmd>lua require("drex.actions.text").copy_name()<CR>',
-            ['Y'] = '<cmd>lua require("drex.actions.text").copy_relative_path()<CR>',
-            ['<C-y>'] = '<cmd>lua require("drex.actions.text").copy_absolute_path()<CR>',
+            ['y'] = { '<cmd>lua require("drex.actions.text").copy_name()<CR>', { desc = 'copy element name' }},
+            ['Y'] = { '<cmd>lua require("drex.actions.text").copy_relative_path()<CR>', { desc = 'copy element relative path' }},
+            ['<C-y>'] = { '<cmd>lua require("drex.actions.text").copy_absolute_path()<CR>', { desc = 'copy element absolute path' }},
         },
         ['v'] = {
             -- file actions
-            ['d'] = ':lua require("drex.actions.files").delete("visual")<CR>',
-            ['r'] = ':lua require("drex.actions.files").multi_rename("visual")<CR>',
+            ['d'] = { ':lua require("drex.actions.files").delete("visual")<CR>', { desc = 'delete elements' }},
+            ['r'] = { ':lua require("drex.actions.files").multi_rename("visual")<CR>', { desc = 'rename elements' }},
             -- add/remove elements from clipboard
-            ['M'] = ':DrexMark<CR>',
-            ['u'] = ':DrexUnmark<CR>',
-            ['m'] = ':DrexToggle<CR>',
+            ['M'] = { ':DrexMark<CR>', { desc = 'mark elements' }},
+            ['u'] = { ':DrexUnmark<CR>', { desc = 'unmark elements' }},
+            ['m'] = { ':DrexToggle<CR>', { desc = 'toggle elements' }},
             -- string copy utilities
-            ['y'] = ':lua require("drex.actions.text").copy_name(true)<CR>',
-            ['Y'] = ':lua require("drex.actions.text").copy_relative_path(true)<CR>',
-            ['<C-y>'] = ':lua require("drex.actions.text").copy_absolute_path(true)<CR>',
+            ['y'] = { ':lua require("drex.actions.text").copy_name(true)<CR>', { desc = 'copy element names' }},
+            ['Y'] = { ':lua require("drex.actions.text").copy_relative_path(true)<CR>', { desc = 'copy element relative paths' }},
+            ['<C-y>'] = { ':lua require("drex.actions.text").copy_absolute_path(true)<CR>', { desc = 'copy element absolute paths' }},
         }
     },
     on_enter = nil,
@@ -162,6 +162,43 @@ local function validate_sorting(sorting)
     return true
 end
 
+---Helper function to check for valid keybinding definitions
+---Keybindings can be a string, a function or a list containing the binding plus an option table (two elements are required)
+---@param keybindings table A list of keybindings to validate
+---@return boolean
+local function validate_keybindings(keybindings)
+    local errors = {}
+    local error_template = 'DREX - mode: %s keybinding: %s - %s'
+
+    for mode, bindings in pairs(keybindings) do
+        for key, binding in pairs(bindings) do
+            if binding then
+                local t = type(binding)
+                if t == 'string' or t == 'function' then
+                    -- do nothing
+                elseif t == 'table' then
+                    if type(binding[1]) ~= 'string' and type(binding[1]) ~= 'function' then
+                        table.insert(errors, error_template:format(mode, key, 'first element has to be a function or string'))
+                    end
+
+                    if type(binding[2]) ~= 'table' then
+                        table.insert(errors, error_template:format(mode, key, 'second element has to be a table'))
+                    end
+                else
+                    table.insert(errors, error_template:format(mode, key, 'value has to be a string, a function or a list ('..t..')'))
+                end
+            end
+        end
+    end
+
+    if #errors > 0 then
+        vim.notify('There are problems with the keybindings, fall back to default!\n'..table.concat(errors, '\n'), vim.log.levels.WARN, { title = 'DREX' })
+        return false
+    end
+
+    return true
+end
+
 M.options = defaults
 
 ---Configure the global DREX settings
@@ -172,6 +209,11 @@ function M.configure(user_config)
     -- overwrite ALL default keybindings if requested
     if M.options.disable_default_keybindings then
         M.options.keybindings = user_config.keybindings
+    end
+
+    -- check for valid keybindings
+    if not validate_keybindings(M.options.keybindings) then
+        M.options.keybindings = defaults.keybindings
     end
 
     -- check for valid icons
@@ -207,21 +249,23 @@ end
 ---Only intended for internal usage within the DREX plugin
 ---@param buffer number Buffer ID
 function M.set_default_keybindings(buffer)
-    local opts = { noremap = true, silent = true, nowait = true }
-
     for mode, bindings in pairs(M.options.keybindings) do
         for lhs, rhs in pairs(bindings) do
             -- check if `rhs` is truthy, users can set rhs to `false` to disable certain default bindings
             if rhs then
-                if type(rhs) == 'function' then
-                    rhs = string.format(
-                        ":lua require('drex.config').options.keybindings['%s']['%s']()<CR>",
-                        mode,
-                        lhs:gsub('<', '<lt>') -- escape keycodes like '<CR>', '<Esc>', etc.
-                    )
+                local opts = {
+                    buffer = buffer,
+                    silent = true,
+                    nowait = true
+                }
+
+                -- check for passed options table
+                if type(rhs) == 'table' then
+                    opts = vim.tbl_extend('force', opts, rhs[2])
+                    rhs = rhs[1]
                 end
 
-                vim.api.nvim_buf_set_keymap(buffer, mode, lhs, rhs, opts)
+                vim.keymap.set(mode, lhs, rhs, opts)
             end
         end
     end
